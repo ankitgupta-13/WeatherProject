bf857c5d1185bd036ac82fd0b8b1e25c9aa837a3
ankitgupta-13 <guptankit0522@gmail.com>
2022-09-30 14:14:52 +0530

    Modified

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const https=require("https");
 const bodyParser=require("body-parser");
+const cors=require("cors");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/public/index.html");
@@ -23,7 +25,7 @@ app.post("/",function(req,res){
             const icon=weather.weather[0].icon;
             const iconUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png"
             res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in "+query+" is : " + weatherdata + "</h1>");
+            res.write("<h1>The temperature in "+query+" is : " + weatherdata + "&#176;C </h1>");
             res.write("<img src="+iconUrl+" />" );
             res.send();
         })
