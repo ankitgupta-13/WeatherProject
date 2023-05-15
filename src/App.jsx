821c70e46833209edc3ef6b803040f418ae937f7
ankitgupta-13 <guptankit0522@gmail.com>
2023-05-15 12:52:10 +0530

    Background videos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { useGeolocated } from "react-geolocated";
+import Search from "../src/assets/search.png";
+import Location from "../src/assets/gps.png";
 
 function App() {
   const [city, setCity] = useState("");
@@ -19,6 +21,7 @@ function App() {
   useEffect(() => {
     if (coords) {
       data();
+      setBackground();
     }
   }, [coords]);
 
@@ -35,29 +38,129 @@ function App() {
           }`
     );
     const data = await response.json();
-    setTemperature(data.main.temp);
-    console.log(data.main.temp);
+    setTemperature(Math.round(data.main.temp));
     setWeather(data.weather[0].main);
+    setBackground(data.weather[0].main);
   };
-
+  const setBackground = (weather) => {
+    if (weather === "Haze") {
+      const video = document.createElement("video");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("loop", "");
+      video.muted = true;
+      video.style.position = "fixed";
+      video.style.top = "50%";
+      video.style.left = "50%";
+      video.style.minWidth = "100%";
+      video.style.minHeight = "100%";
+      video.style.width = "auto";
+      video.style.height = "auto";
+      video.style.zIndex = "-1";
+      video.style.transform = "translate(-50%, -50%)";
+      const source = document.createElement("source");
+      source.setAttribute("src", "haze.mp4");
+      video.appendChild(source);
+      document.body.appendChild(video);
+    } else if (weather === "Clear") {
+      const video = document.createElement("video");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("loop", "");
+      video.muted = true;
+      video.style.position = "fixed";
+      video.style.top = "10%";
+      video.style.left = "50%";
+      video.style.minWidth = "100%";
+      video.style.minHeight = "100%";
+      video.style.width = "auto";
+      video.style.height = "auto";
+      video.style.zIndex = "-1";
+      video.style.transform = "translate(-50%, -50%)";
+      const source = document.createElement("source");
+      source.setAttribute("src", "clear.mp4");
+      video.appendChild(source);
+      document.body.appendChild(video);
+    } else if (weather === "Clouds") {
+      const video = document.createElement("video");
+      video.loop = true;
+      video.autoplay = true;
+      video.muted = true;
+      video.style.position = "fixed";
+      video.style.top = "50%";
+      video.style.left = "50%";
+      video.style.minWidth = "100%";
+      video.style.minHeight = "100%";
+      video.style.width = "auto";
+      video.style.height = "auto";
+      video.style.zIndex = "-1";
+      video.style.transform = "translate(-50%, -50%)";
+      const source = document.createElement("source");
+      source.setAttribute("src", "clouds.mp4");
+      video.appendChild(source);
+      document.body.appendChild(video);
+    } else if (weather === "Rain") {
+      const video = document.createElement("video");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("loop", "");
+      video.muted = true;
+      video.style.position = "fixed";
+      video.style.top = "50%";
+      video.style.left = "50%";
+      video.style.minWidth = "100%";
+      video.style.minHeight = "100%";
+      video.style.width = "auto";
+      video.style.height = "auto";
+      video.style.zIndex = "-1";
+      video.style.transform = "translate(-50%, -50%)";
+      const source = document.createElement("source");
+      source.setAttribute("src", "rain.mp4");
+      video.appendChild(source);
+      document.body.appendChild(video);
+    } else if (weather === "Snow") {
+      const video = document.createElement("video");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("loop", "");
+      video.muted = true;
+      video.style.position = "fixed";
+      video.style.top = "50%";
+      video.style.left = "50%";
+      video.style.minWidth = "100%";
+      video.style.minHeight = "100%";
+      video.style.width = "auto";
+      video.style.height = "auto";
+      video.style.zIndex = "-1";
+      video.style.transform = "translate(-50%, -50%)";
+      const source = document.createElement("source");
+      source.setAttribute("src", "snow.mp4");
+      video.appendChild(source);
+      document.body.appendChild(video);
+    }
+  };
   return (
-    <>
+    <div className="container">
       <h1>Weather App</h1>
-      <label htmlFor="">Enter City</label>
-      <input
-        type="text"
-        onChange={(event) => {
-          setCity(event.target.value);
-        }}
-        value={city}
-      />
-      <button onClick={handleGetWeather}>Get Weather</button>
-      <div>
-        <h2>City Name {city}</h2>
-        <h2>Temperature {temperature} </h2>
-        <h2>Weather {weather}</h2>
+      <div className="input">
+        <img src={Location} alt="" className="locationIcon" />
+        <input
+          type="text"
+          placeholder="Enter City Name"
+          onChange={(event) => {
+            setCity(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleGetWeather();
+            }
+          }}
+          value={city}
+        />
+        <img src={Search} alt="Not found" className="searchIcon" />
       </div>
-    </>
+      <div className="output">
+        {/* <h2>City Name {city}</h2> */}
+        <div>Temperature : {temperature}°C </div>
+        <div>Weather : {weather}</div>
+      </div>
+    </div>
   );
 }
 
